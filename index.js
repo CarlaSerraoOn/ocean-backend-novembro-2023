de118f1a4ab9c35d4583511291a24f7af9ff6dd0
Carla Serrão <104877365+CarlaSerraoOn@users.noreply.github.com>
2023-11-09 14:41:43 +0000

    Resposta do metódo post

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 
+// Registrar um Middleware
+// Indica que todas as requisições podem receber
+// Body em JSON. A partir disso, o express aplica
+// um JSON
+
+app.use(express.json())
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
@@ -11,28 +18,40 @@ app.get("/oi", function (req, res) {
 
 const lista = ["Harry Potter", "Hermione Granger", "Ronny Wesley"]
 
-//Read All - [GET] /item
+// Read All - [GET] /item
 app.get("/item", function (req,res){
     res.send(lista)
 })
 
-//Read by id - [GET] /item/:id
+// Read by id - [GET] /item/:id
 app.get("/item/:id", function (req,res) {
-//Pegamos o ID de rota e subtraimos 1 para ficar
-//Equivalente ao índice da lista que começa em 0
+// Pegamos o ID de rota e subtraimos 1 para ficar
+// Equivalente ao índice da lista que começa em 0
     const id = req.params.id -1
 
-//acessamos o item na lista, usando o índice corrigido  
+// Acessamos o item na lista, usando o índice corrigido  
     const item = lista[id]
 
-//Enviamos o intem como resposta do endpoint
+// Enviamos o intem como resposta do endpoint
     res.send(item)
+})
+
+// Creat - [POST] /item
+app.post("/item", function (req,res) {
+
+// Extraimos o nome do Body da requisição
+    const item = req.body.nome
+
+// Adicionamos o item recebido na lista    
+    lista.push(item)
+
+// Exibimos uma mensagem de sucesso
+    res.send("Item adicionado com o sucesso!")
 })
 
 
 
 
 
-app.listen(3000 ,() =>
-    console.log("Servidor rodando em http://localhost:3000")
-);
+
+app.listen(3000);
